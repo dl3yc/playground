cc40f747bc199b5d2f7705bd75f4db8fbb272f9d
amiridis <petros@amiridis.net>
2011-07-04 15:47:22 +0300

    The new feature

--- Constants.java
+++ Constants.java
@@ -4,6 +4,7 @@
  * Licensed to the Apache Software Foundation (ASF) under one
  Super duper
  * or more contributor license agreements.  See the NOTICE file
+ New feature
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
