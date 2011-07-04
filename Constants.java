da42cc6082c570fbad72ec962480b0dc88c05ed7
amiridis <petros@amiridis.net>
2011-07-04 15:40:45 +0300

    The best feature

--- Constants.java
+++ Constants.java
@@ -2,6 +2,7 @@
  amiridis-test
  amiridis
  * Licensed to the Apache Software Foundation (ASF) under one
+ Super duper
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
