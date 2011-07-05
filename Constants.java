242283d44fb699433f1025e872f01f7158f2e9e1
amiridis-test <amiridis@gmail.com>
2011-07-05 01:56:00 -0700

    Wow amiridis-test

--- Constants.java
+++ Constants.java
@@ -19,6 +19,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ amiridis-test
  */
 package org.ops4j.pax.swissbox.tinybundles.dp;
 
